ef4ec36cc91a83d4d9886405e84a245229c75422
Greg Heiman <gregheiman02@gmail.com>
2020-04-10 22:28:24 -0500

    Initial Commit

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <text>Hello World</text>
+            </div>
+        )        
+    }
+}
+
+export default App;
